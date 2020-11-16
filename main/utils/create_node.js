b63ff8072f1f291b95fdd0dc268d6ab7a757c817
K9714 <kskim.hci@gmail.com>
2020-11-16 16:13:34 +0900

    create node update

--- main/utils/create_node.js
+++ main/utils/create_node.js
@@ -5,28 +5,30 @@
     Desc. 
             Node-red 노드를 쉽게 만들 수 있는 어시스트 파일입니다.
 */
+const { exception } = require('console');
 const fs = require('fs');
 
 node = {}
 // 노드가 저장될 폴더
 NODE_SAVE_PATH = __dirname + "/../nodes/";
-
+// 저장될 폴더명
+NODE_SAVE_PATH_NAME = "TestNode";
 // json 데이터
-node.name = "continues-mnist-dataset";
+node.name = "test_node";
 node.version = "1.0.0";
-node.description = "hci-continues mnist dataset node";
+node.description = "hci code_viewer node";
 node.main = "index.js";
-node.keywords = ["continues-mnist-dataset"];
+//node.keywords = ["code_viewer"];
 node.author = "HCI";
 node.license = "UNLICENSED";
 
 // Node-red 연결 js 파일
-NODE_RED_JS = "continues-mnist-dataset.js";
+NODE_RED_JS = ["test_node1", "test_node2"];
 // Node-red js 파일 함수 명
-NODE_RED_FUNC_NAME = "CONTI_MNIST_Dataset"
+NODE_RED_FUNC_NAME = ["TestNode1", "TestNode2"]
 
 function createNode(node){
-    dir = NODE_SAVE_PATH + node.name
+    dir = NODE_SAVE_PATH + NODE_SAVE_PATH_NAME;
     // 경로가 없으면 만듬
     if (!fs.existsSync(dir)) {
         fs.mkdirSync(dir);
@@ -35,26 +37,32 @@ function createNode(node){
     dir = dir + '/';
     node["node-red"] = {}
     node["node-red"].nodes = {};
-    node["node-red"].nodes[node.name] = NODE_RED_JS;
-    json = JSON.stringify(node);
-    html = fs.readFileSync(__dirname + '/temp_html.txt').toString();
-    html = replaceNodeString(node, html);
-    js = fs.readFileSync(__dirname + '/temp_js.txt').toString();
-    js = replaceNodeString(node, js);
-    // SAVE
+    
+    NODE_RED_JS.forEach(function(e, i) {
+        node["node-red"].nodes[e] = e + ".js";
+        html = fs.readFileSync(__dirname + '/temp_html.txt').toString();
+        html = replaceNodeString(i, node, html);
+        js = fs.readFileSync(__dirname + '/temp_js.txt').toString();
+        js = replaceNodeString(i, node, js);
+        
+        // js
+        fs.writeFileSync(dir + e + ".js", js, {encoding : 'utf8'});
+        
+        // html
+        fs.writeFileSync(dir + e + '.html', html, {encoding : 'utf8'});
+        
+    });
     // json
+    json = JSON.stringify(node);
     fs.writeFileSync(dir + 'package.json', json);
-    // js
-    fs.writeFileSync(dir + NODE_RED_JS, js, {encoding : 'utf8'});
-    // html
-    fs.writeFileSync(dir + node.name + '.html', html, {encoding : 'utf8'});
     return;
 }
 
-function replaceNodeString(node, str) {
-    str = str.replace(/\<NODE_NAME\>/g, '"' + node.name + '"');
+function replaceNodeString(i, node, str) {
+    str = str.replace(/\<NODE_NAME\>/g, '"' + NODE_RED_JS[i] + '"');
     str = str.replace(/\<NODE_DESC\>/g, '"' + node.description + '"');
-    str = str.replace(/\<FUNC_NAME\>/g, NODE_RED_FUNC_NAME);
+    str = str.replace(/\<FUNC_NAME\>/g, NODE_RED_FUNC_NAME[i]);
     return str;
 }
 createNode(node);
+
